e9f3ae65959a9d7c4e26f781cd5e79e11577f995
t9uo <t9uo@fresnou.org>
2023-09-05 09:04:47 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 module.exports = router;
+router.post('/', function(req, res){
+  res.render('index', {
+    phrase: req.body.userText,
+    message: getResultDescription(req.body.userText)
+  })
+})
+function checkPalindrome(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("")
+  if (phrase.toLowerCase().replace(" ", "") === tempt.toLowerCase().replace(" ", "") ){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function getResultDescription (phrase){
+  if (checkPalindrom(phrase)) {
+    return `${phrase} is a palindrome.`
+  }
+  return `${phrase} is not a palindrome.`
+}
